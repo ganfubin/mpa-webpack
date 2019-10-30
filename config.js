4c81e3502ae53c1a4b30a415fb88ba0f1a1c756f
ganfubin <ganfubin@zbj.com>
2019-10-30 17:24:35 +0800

    init code

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  iconFontCssUrl: `https://at.alicdn.com/t/font_1145510_krlx5ac9z.css?t=${Date.now()}`,
-  iconFontJsUrl: `https://at.alicdn.com/t/font_1145510_krlx5ac9z.js?t=${Date.now()}`,
-  domain: '.ustax.com.cn',
-  DP: '0a13871135c684f55f89bd1103f790d9'
+
 }
